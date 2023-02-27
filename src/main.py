f479763602f4820316e9d847e81194dfff1b394e
Katya Fadeeva <katfadmail@gmail.com>
2023-02-27 22:51:57 +0200

    work with keyboard

--- src/main.py
+++ src/main.py
@@ -15,11 +15,33 @@ from kivy.core.text import LabelBase
 
 import re
 
+class KeyboardListener(Widget):
+
+    def __init__(self, **kwargs):
+        super(KeyboardListener, self).__init__(**kwargs)
+        self._keyboard = Window.request_keyboard(self._keyboard_closed, self, 'text')
+        self._keyboard.bind(on_key_down=self._on_keyboard_down)
+
+    def _keyboard_closed(self):
+        print('Keyboard have been closed!')
+        self._keyboard.unbind(on_key_down=self._on_keyboard_down)
+        self._keyboard = None
+
+    def _on_keyboard_down(self, keyboard, keycode, text, modifiers):
+        print(self.text)
+
+        return True
+    
+    def get_cmnd_name(cmnd):
+        pass
+
+
 class CashFlowLayout(BoxLayout):
     pass
 
 class MainMenuLayout(BoxLayout):
-    pass
+    def on_button_click(self, widget):
+        print(clean_text(widget.text))
 
 
 def clean_text(dirty):
