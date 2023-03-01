f8764b24972855978f85bcbacad23982d51c48cd
Katya Fadeeva <katfadmail@gmail.com>
2023-03-01 20:55:56 +0200

    delete __pycache__

--- src/helpers.py
+++ src/helpers.py
@@ -1,14 +1,32 @@
 import re
 
+COMMANDS = {
+    'main_menu' : ['si', 'sp', 'cf', 'pm', 'c', 'f', 'o', 'm'],
+    'si'  : [],
+    'sp'  : [],
+    'cf'  : [],
+    'pm'  : [],
+    'c' : [],
+    'f'   : [],
+    'o'   : [],
+    'm'   : [],
+    'ignore_keycode' : [300, 301, 303, 304, 305, 306] # numlock, rshift, rctrl, lshift, rshift
+}
+
+## NEED add enter after each command
 full_cmnd = []
 def get_cmnd_name(screen_name, cmnd):
-    cmnd_list = {
-        'main_menu': ['si', 'sp', 'cf', 'pm', 'c', 'f', 'o', 'm']
-    }
-    if screen_name in cmnd_list.keys() and cmnd[0] not in (301, 303, 304, 305, 306):
-        if len(full_cmnd) == 2: full_cmnd.clear()
+    if screen_name in COMMANDS.keys() and cmnd[0] not in COMMANDS['ignore_keycode']:
+        if cmnd[0] == 13 : full_cmnd.clear()
         full_cmnd.append(cmnd[1])
-    print(full_cmnd)
+        
+        ans = [''.join(full_cmnd)]
+        if ans[0] in COMMANDS[screen_name]:
+            ans.append('Yes')
+        else:
+            ans.append('No')
+            
+    print(ans)
 
 def clean_text(dirty):
     dirty = dirty.replace('[/font]', '')
